940b0d8dd3347648a3829f93a7641542d5d6bdaf
Ben Buttigieg <bebuttig@microsoft.com>
2019-03-12 15:37:42 +0100

    Fly controll workign.

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,37 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  bool _isFlapping =false;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, lowerBound: -1.0, upperBound: 1.01, debugLabel: "Drop", duration: Duration(seconds: 5),)
+    ..addListener(() {
+      print("Frame "+ _controller.toStringDetails());
+      this.setState(() {});
+    })..addStatusListener((status){
+      print("Status changed " + status.toString() + _controller.toStringDetails());
+      if (status == AnimationStatus.completed) {
+          print("Drop completed" + _controller.toStringDetails());
+      }
+    });
+  }
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Stack(children: <Widget>[
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
         Container(
       decoration: BoxDecoration(
               // Box decoration takes a gradient
@@ -35,49 +59,50 @@ class _AppState extends State<App> with SingleTickerProviderStateMixin {
                 stops: [0.1, 1.0],
                 colors: [
                   Color.fromRGBO(131, 232, 255, 1.0),
-                  Color.fromRGBO(110, 10, 211, 1.0),
+                  Color.fromRGBO(110, 90, 210, 1.0),
                 ],
               ),
           ),
        ),
-       Bird()]
+       FractionallySizedBox(widthFactor: 0.2, heightFactor: 0.2, child:
+        FlareActor("assets/FlutterBird.flr", fit:BoxFit.fitWidth, animation:"Flap", isPaused: !_isFlapping),
+
+        alignment: new Alignment(0.0, _controller.value),
+        ),
+       GestureDetector(
+        onTap: (){
+          _isFlapping=true;
+         double top =_controller.value - 0.25;
+         print("Flying" + _controller.toStringDetails());
+         _controller.animateTo(top, duration: Duration(milliseconds: 200)).whenComplete((){
+          _isFlapping =false;
+         print("Flap completed" + _controller.toStringDetails());
+        _controller.animateWith(GravitySimulation(9.8, _controller.value, 1.01,1.0));
+        }
+        );
+        }),
+        ]
        ),
+       
     );
   }
 }
 
-class Bird extends StatefulWidget {
-  @override
-  _BirdState createState() => _BirdState();
-}
+// class Bird extends StatefulWidget {
+//   Bird({this.y});
+//   double y;
+//   @override
+//   _BirdState createState() => _BirdState();
+// }
 
-class _BirdState extends State<Bird> with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this, upperBound: 1.0, debugLabel: "Drop", duration: Duration(seconds: 10), lowerBound: 0.0)..addListener(() {
-      this.setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    return Container(width: 220.0, height: 200.0, 
-        child: GestureDetector(child: FlareActor("assets/FlutterBird.flr", alignment: Alignment.topCenter, fit:BoxFit.fill, animation:"Flap", isPaused: false),
-        onTap: (){
-          _controller.reset();
-          _controller.animateWith(GravitySimulation(10.0,0.0,300.0,1.0));
-        }),
-        transform: Matrix4.identity()..translate(0.0, _controller.value * height / 2),
-        );
-  }
-}
+// class _BirdState extends State<Bird> with SingleTickerProviderStateMixin {
+//   @override
+//   Widget build(BuildContext context) {
+//     double height = MediaQuery.of(context).size.height;
+//     double x = MediaQuery.of(context).size.width / 9;
+//     return Container(width: 250.0, height: 200.0, 
+//         child: FlareActor("assets/FlutterBird.flr", alignment: Alignment.topCenter, fit:BoxFit.fill, animation:"Flap", isPaused: false),
+//         transform: Matrix4.identity()..translate(x, widget.y),
+//         );
+//   }
+// }
